26067e1a0f35c3dfb0a2198e3192c0b4ad03a2f1
mazdak <nazemi.works@gmail.com>
2019-05-19 20:43:33 -0700

    make reusability with usage of functional programming

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,23 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { link } from 'fs';
-
+//reusability
+const useResources = resource => {
+  const [resources, setResources] = useState([]);  //useEffect as lifeCycle methods cdm and cdu
+  useEffect(() => {
+    (async resource => {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+      setResources(response.data);
+    })(resource)
+  }, [resource]) //can user iiFe
+  return resources;
+}
+//main comp
 const ResourceList = ({ resource }) => {
-  const [resources, setResources ] = useState([]);
-  //useEffect as lifeCycle methods cdm and cdu
-  const fetchResource = async resource => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
-    setResources(response.data);
-  }
-  //this arr is like prevProps first check it if had change that http run
-  //if put no arr as sec func always call like spam
-  useEffect(()=> {fetchResource(resource)}, [resource]) //can user iiFe
+  const resources = useResources(resource);
   return (
     <ul>
-      {
-        resources.map(record =>
-          <li key={record.id}>{record.title}</li>)
-      }
+      {resources.map(record =><li key={record.id}>{record.title}</li>)}
     </ul>
   );
 }
