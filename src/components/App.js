881f935f1ffc66323f0875a02291f4fab7a244f0
mazdak <nazemi.works@gmail.com>
2019-05-19 19:28:08 -0700

    first init of hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
+
 
 const App = () => {
+
+  const [resource, setResource] = useState('posts');
+
   return (
     <div>
-      df
+      <div>
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('todos')}>Todos</button>
+      </div>
+      {resource}
     </div>
   )
 }
 
 export default App
+
